2299b97d756e08e8fea5383cfdbbaa5abc6527d2
guillermo.carrillo1 <guillermo.carrillo@nter.es>
2024-07-24 15:37:24 +0200

    created project models before generating or sending migrations

--- speedtest/urls.py
+++ speedtest/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('api/v1', include('api.urls',namespace='api')),
+    path('api/v1', include('api.urls', namespace = 'api')),
     path('profiles/', include('profiles.urls')),
     path('ranking/', include('ranking.urls')),
 ]
